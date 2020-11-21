81768da404660445fcc4e89dafe339076c07500c
AlexBuratta13 <alex,buratta13@gmail.com>
2020-11-21 16:50:01 +0100

    Add project files.

---
+++ DisegnoGeometrico/Punti2D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DisegnoGeometrico
+{
+    public class Punti2D
+    {
+        public int x { get; private set; }
+        public int y { get; private set; }
+        public Punti2D(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ DisegnoGeometrico/Punti3D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DisegnoGeometrico
+{
+    public class Punti3D : Punti2D
+    {
+        public int z { get; private set; }
+        public Punti3D(int x,int y,int z) : base(x, y)
+        {
+            this.z = z;
+        }
+    }
+}
